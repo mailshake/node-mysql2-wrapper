1226c9c1b0148d5eb6f9efdce147cb492a4bd5a3
Colin Mathews <colin.mathews@gmail.com>
2016-04-08 12:05:54 -0400

    Extend drop to support multiple tables

--- lib/util/drop.ts
+++ lib/util/drop.ts
@@ -1,7 +1,7 @@
 import { Promise } from 'es6-promise';
 import MySQL from '../services/mysql';
 
-export default function dropTable(sql: MySQL, tableName: string): Promise<any> {
-  let query = `drop table if exists ${tableName};`;
+export default function dropTables(sql: MySQL, ...tableNames: string[]): Promise<any> {
+  let query = `drop table if exists ${tableNames.join(', ')};`;
   return sql.singleTransaction(query);
 }
